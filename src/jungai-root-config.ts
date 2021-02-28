e8a9fefce2d9db21ec9070bf1a24b464771d32f4
jungai <juworaphol125@gmail.com>
2021-02-28 15:10:11 +0700

    update deps for vue & react app

--- src/jungai-root-config.ts
+++ src/jungai-root-config.ts
@@ -3,7 +3,13 @@ import { registerApplication, start } from "single-spa";
 registerApplication({
   name: "@jungai/micro-vue3",
   app: () => System.import("@jungai/micro-vue3"),
-  activeWhen: ["/"],
+  activeWhen: ["/vue"],
+});
+
+registerApplication({
+  name: "@jungai/micro-react",
+  app: () => System.import("@jungai/micro-react"),
+  activeWhen: ["/react"],
 });
 
 start({
